530781a982351728f52bb62d1cc2944d5c3419aa
Ruslanas Balčiūnas <ruslanas.com@gmail.com>
2016-04-01 12:51:33 +0300

    Setup database for ACL test

--- tests/AclTest.php
+++ tests/AclTest.php
@@ -2,32 +2,40 @@
 
 use Stream\App;
 use Stream\Acl;
+use Stream\Test\DatabaseTestCase;
 
-class AclTest extends PHPUnit_Framework_TestCase {
+class AclTest extends DatabaseTestCase {
+	
 	public function setUp() {
-		$app = new App();
-		$app->loadConfig();
-		$app->connect('test_stream');
+		parent::setUp();
+		$this->acl = new Acl();
 	}
+
 	public function testAllow() {
-		$acl = new Acl();
-		$this->assertTrue($acl->allow('GET', '/'));
-		$this->assertTrue($acl->allow('GET', '/user/login'));
-		$this->assertFalse($acl->allow('DELETE', '/user/1'));
-		$this->assertTrue($acl->allow('POST', '/user/add'));
-		$this->assertFalse($acl->allow('POST', '/clients.json'));
-		
+
+		$this->assertTrue($this->acl->allow('GET', '/'));
+		$this->assertTrue($this->acl->allow('GET', '/user/login'));
+		$this->assertFalse($this->acl->allow('DELETE', '/user/1'));
+		$this->assertTrue($this->acl->allow('POST', '/user/add'));
+		$this->assertFalse($this->acl->allow('POST', '/clients.json'));
+	}
+
+	public function testAllowAdmin() {		
 		// admin can create and delete
 		$_SESSION['uid'] = 1;
-		$this->assertTrue($acl->allow('GET', '/'));
-		$this->assertTrue($acl->allow('POST', '/clients.json'));
-		$this->assertTrue($acl->allow('DELETE', '/clients/1.json'));
-		
+		$this->assertTrue($this->acl->allow('GET', '/'));
+		$this->assertTrue($this->acl->allow('POST', '/clients.json'));
+		$this->assertTrue($this->acl->allow('DELETE', '/clients/1.json'));
+	}
+
+	public function testAllowManager() {
+
 		// can't create post, but can add client
 		$_SESSION['uid'] = 2;
-		$this->assertTrue($acl->allow('POST', '/clients.json')); // ???
-		$this->assertFalse($acl->allow('DELETE', '/clients/1.json'));
-		$this->assertFalse($acl->allow('POST', '/posts.json'));
-		$this->assertFalse($acl->allow('PUT', '/clients.json'));
+		$this->assertTrue($this->acl->allow('POST', '/clients.json')); // ???
+		$this->assertFalse($this->acl->allow('DELETE', '/clients/1.json'));
+		$this->assertFalse($this->acl->allow('POST', '/posts.json'));
+		$this->assertFalse($this->acl->allow('PUT', '/clients.json'));
+
 	}
 }
